419c14f80b856b40cabe7161f5f167af5f8c3a61
Elisa <elisalel-1998@hotmail.com>
2022-10-15 15:02:27 +0200

    components and pages template

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.scss';
+import PaginaProva from './pages/PaginaProva';
+import './styles/base.scss';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <PaginaProva/>
     </div>
   );
 }
---
+++ client/src/components/ComponenteProva/ComponenteProva.js
@@ -0,0 +1,10 @@
+import styles from "./index.module.scss";
+
+export default function ComponenteProva(props) {
+
+  return (
+    <div className={styles.wrap}>
+      <span className={styles.titolo}>provo</span>
+    </div>
+  )
+}
