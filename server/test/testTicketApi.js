68540eb8691b84c37dd7671809c5b68e866f08cc
giuseppelazzara98 <s302064@studenti.polito.it>
2022-10-18 18:32:13 +0200

    tests about estimation of the time

--- server/test/testTicketApi.js
+++ server/test/testTicketApi.js
@@ -25,14 +25,20 @@ describe('test ticket apis', () => {
     getClientsPerService(200,4,value4);
 
     //Testing POST /api/ticket
-    newTicket(201,3);
-    newTicket(201,1);
-    newTicket(404,7);
-    newTicket(201,2);
-    newTicket(201,2);
-    newTicket(404,78);
-    newTicket(201,4);
-    newTicket(404,5);
+    newTicket(201,3,{ waitListCode: 15, queueCode: 'Service 3', timeEstimation: 148 });
+    newTicket(201,1,{ waitListCode: 16, queueCode: 'Service 1', timeEstimation: 25 });
+    newTicket(404,7,{
+        error: "Not Found"
+    });
+    newTicket(201,2,{ waitListCode: 17, queueCode: 'Service 2', timeEstimation: 62.5 });
+    newTicket(201,2,{ waitListCode: 18, queueCode: 'Service 2', timeEstimation: 77.5 });
+    newTicket(404,78,{
+        error: "Not Found"
+    });
+    newTicket(201,4,{ waitListCode: 19, queueCode: 'Service 4', timeEstimation: 185 });
+    newTicket(404,5,{
+        error: "Not Found"
+    });
 
     getClientsPerService(200,1,(value1 + 1));
     getClientsPerService(200,2,(value2 + 2));
@@ -61,13 +67,16 @@ function getClientsPerService(expectedHTTPStatus, service_id, number) {
     });
 }
 
-function newTicket(expectedHTTPStatus, id_service) {
+function newTicket(expectedHTTPStatus, id_service,counter) {
     it('Inserting a new ticket', function (done) {
        let ticket = { id_service: id_service }
        agent.post('/api/ticket')
             .send(ticket)
             .then(function (res) {
                 res.should.have.status(expectedHTTPStatus);
+                assert.equal(res.body.waitListCode,counter.waitListCode);
+                assert.equal(res.body.queueCode,counter.queueCode);
+                assert.equal(res.body.timeEstimation,counter.timeEstimation);
                 done();
             });
     });
