242f054360bad123f5b6c7b78993388ba7409ceb
FRANCESCO ROSATI s296247 <s296247@studenti.polito.it>
2022-10-18 18:51:56 +0200

    Added user router

--- server/server.js
+++ server/server.js
@@ -4,13 +4,9 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const passport = require('passport'); // auth middleware
-const LocalStrategy = require('passport-local').Strategy; // username and password for login
-const session = require('express-session'); // enable sessions
-const userDao = require('./modules/user-dao'); // module for accessing the users in the DB
-
 const serviceRouter = require('./routers/service_router');
 const ticketRouter = require('./routers/ticket_router');
+const userRouter = require('./routers/user_router');
 
 // init express
 const app = new express();
@@ -24,106 +20,14 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-/*** Set up Passport ***/
-// set up the "username and password" login strategy
-// by setting a function to verify username and password
-passport.use(
-	new LocalStrategy(function (username, password, done) {
-		userDao.getUser(username, password).then((user) => {
-			if (!user) {
-				return done(null, false, {
-					message: 'Incorrect username and/or password.'
-				});
-			}
-			return done(null, user);
-		});
-	})
-);
-
-// serialize and de-serialize the user (user object <-> session)
-// we serialize the user id and we store it in the session: the session is very small in this way
-passport.serializeUser((user, done) => {
-	done(null, user.id);
-});
-
-// starting from the data in the session, we extract the current (logged-in) user
-passport.deserializeUser((id, done) => {
-	userDao
-		.getUserById(id)
-		.then((user) => {
-			done(null, user); // this will be available in req.user
-		})
-		.catch((err) => {
-			done(err, null);
-		});
-});
-
 app.use(morgan('dev'));
 app.use(express.json());
-
-// custom middleware: check if a given request is coming from an authenticated user
-const isLoggedIn = (req, res, next) => {
-	if (req.isAuthenticated()) return next();
-
-	return res.status(401).json({ error: 'User not authenticated' });
-};
-
-// set up the session
-app.use(
-	session({
-		// by default, Passport uses a MemoryStore to keep track of the sessions
-		secret:
-			'a secret sentence not to share with anybody and anywhere, used to sign the session ID cookie',
-		resave: false,
-		saveUninitialized: false
-	})
-);
-
-// then, init passport
-app.use(passport.initialize());
-app.use(passport.session());
 
 /******API******/
 
 app.use('/api',serviceRouter);
 app.use('/api',ticketRouter);
-
-/*** Users APIs ***/
-
-// POST /sessions
-// login
-app.post('/api/sessions', function (req, res, next) {
-	passport.authenticate('local', (err, user, info) => {
-		if (err) return next(err);
-		if (!user) {
-			// display wrong login messages
-			return res.status(401).json(info);
-		}
-		// success, perform the login
-		req.login(user, (err) => {
-			if (err) return next(err);
-
-			// req.user contains the authenticated user, we send all the user info back
-			// this is coming from userDao.getUser()
-			return res.json(req.user);
-		});
-	})(req, res, next);
-});
-
-// DELETE /sessions/current
-// logout
-app.delete('/api/sessions/current', (req, res) => {
-	req.logout();
-	res.end();
-});
-
-// GET /sessions/current
-// check whether the user is logged in or not
-app.get('/api/sessions/current', (req, res) => {
-	if (req.isAuthenticated()) {
-		res.status(200).json(req.user);
-	} else res.status(401).json({ error: 'Unauthenticated user!' });
-});
+app.use('/api',userRouter);
 
 // activate the server
 app.listen(port, () => {
