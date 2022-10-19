ca5bfc8f89dbaaa9491a50780be976b43e3a87e1
Elisa <elisalel-1998@hotmail.com>
2022-10-19 20:42:45 +0200

    fix navigation navbar

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,24 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate, 
-    Link
-} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function NavbarHead() {
+  const navigate = useNavigate();
   return (
     <>
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand >Queue Manager</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/mainboard">MainBoard</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/">User Page</Nav.Link>
+            <Nav.Link onClick={() => navigate("/mainboard")}>MainBoard</Nav.Link>
+            <Nav.Link onClick={() => navigate("/login")}>Login</Nav.Link>
+            <Nav.Link onClick={() => navigate("/")}>User Page</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
