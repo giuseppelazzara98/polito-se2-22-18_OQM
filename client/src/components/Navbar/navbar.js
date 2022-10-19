f99a8791f89deeeb0d4fd919783e9eefd6b486c6
Elisa <elisalel-1998@hotmail.com>
2022-10-19 20:48:41 +0200

    fix merge

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useNavigate } from 'react-router-dom';
 
-function NavbarHead() {
+function NavbarHead(props) {
   const navigate = useNavigate();
   return (
     <>
